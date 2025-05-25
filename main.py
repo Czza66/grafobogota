eb5ca2dc0c850d80d65528ca8a7715e64d451578
Juan Sebastian Briñez Capera <jsbrinez@ucompensar.edu.co>
2025-05-25 14:41:53 -0500

    segundo

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 )
 
 # Cargar el grafo de Bogotá
-G = ox.load_graphml("bogota.graphml")
+G = ox.graph_from_place("Bogotá, Colombia", network_type="drive")
 
 @app.post("/ruta")
 async def calcular_ruta(coordenadas: list[str]):
